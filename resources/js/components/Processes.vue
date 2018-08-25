12d07da2c6d1b9e7246d5f079447d40bc06b370a
Jeff Kreitner <kreitje@gmail.com>
2018-08-25 08:19:53 -0400

    Added the processes card

---
+++ resources/js/components/Processes.vue
@@ -0,0 +1,73 @@
+<template>
+    <card class="flex flex-col items-center justify-center relative nova-horizon-stats-card" v-bind:class="borderClass">
+        <div class="px-3 py-3">
+            <h1 class="text-center text-3xl text-80"><strong>{{stat}}</strong></h1>
+            <h2 class="text-center text-80 font-light">Processes</h2>
+
+            <div v-if="!isHorizonOnline" class="text-danger text-center">Horizon is not online.</div>
+            <div v-if="isError" class="text-sm pt-2">Error getting stats. Is horizon installed?</div>
+        </div>
+
+        <div class="loading-indicator text-sm text-80" v-if="isLoading">Loading...</div>
+        <div class="online-indicator">
+            <span v-if="isHorizonOnline" class="text-success" title="Horizon is running">✔</span>
+            <span v-if="!isHorizonOnline" class="text-danger" title="Horizon is inactive">&times;</span>
+        </div>
+    </card>
+</template>
+
+<script>
+    export default {
+        props: ['card'],
+
+        data: function() {
+            return {
+                isLoading: true,
+                isError: false,
+                stat: 0,
+                status: '',
+                refreshTime: 30
+            }
+        },
+
+        mounted() {
+            if (this.card.refreshTime) {
+                this.refreshTime = parseInt(this.card.refreshTime);
+            }
+
+            this.fetchStats();
+            setInterval(this.fetchStats, this.refreshTime * 1000);
+        },
+
+        computed: {
+            borderClass() {
+
+                if (!this.isLoading && this.isError) {
+                    return 'border border-danger';
+                }
+
+                return '';
+            },
+
+            isHorizonOnline() {
+                return this.status === 'running';
+            }
+        },
+
+        methods: {
+            fetchStats() {
+                this.isLoading = true;
+                this.isError = false;
+                Nova.request().get('/horizon/api/stats').then(response => {
+                    this.isLoading = false;
+                    this.isError = false;
+                    this.stat = response.data.processes;
+                    this.status = response.data.status;
+                }).catch(err => {
+                    this.isLoading = false;
+                    this.isError = true;
+                });
+            }
+        }
+    }
+</script>
